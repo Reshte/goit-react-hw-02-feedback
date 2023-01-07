7285a68fcb71785e7e1244c78b41cb874422e177
Reshte <107108359+Reshte@users.noreply.github.com>
2023-01-07 12:46:14 +0200

    edits

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
 
 export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-  if(good || neutral || bad){   return (
-        <div>
+  if (good || neutral || bad) {
+    return (
+      <div>
         <h2>Statistic</h2>
-          <p>Good: {good}</p>
-          <p>Neutral: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>Total: {total}</p>
-          <p>Positive Feedback: {positivePercentage}</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive Feedback: {positivePercentage} %</p>
       </div>
-  )
-  } else {return (
-    <h2>No feedback given</h2>)
-    }
- 
-
+    )
+  } else {
+    return (
+      <h2>No feedback given</h2>)
+  }
 }
 
 Statistics.propTypes = {
